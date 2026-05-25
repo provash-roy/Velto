54dda6d07c9f6b97010c80ebbcc6319de3a3626b
Provash-roy <provashray94@gmail.com>
2026-05-25 21:45:48 +0600

    add clerk authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
+}
